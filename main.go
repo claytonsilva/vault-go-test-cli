abc989e3da0396fdcdef387d2866df49f94ef158
claytonsilva <clayton@xdevel.com.br>
2018-05-05 02:04:30 -0300

    feat: finished sample app

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
-// create a vault client
-client, err := api.NewClient(&api.Config{Address: url, HttpClient: httpClient})
-if err != nil {
-    panic(err)
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/hashicorp/vault/api"
+)
+
+func callHello() {
+	httpClient := &http.Client{}
+	url := os.Getenv("VAULT_ADDR")
+
+	// create a vault client
+	client, err := api.NewClient(&api.Config{Address: url, HttpClient: httpClient})
+	if err != nil {
+		panic(err)
+	}
+
+	// the login path
+	// this is configurable, change userpass to ldap etc
+	path := "secret/sample/hello"
+
+	// PUT call to get a token
+	secret, err := client.Logical().Read(path)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(secret.Data["hello"])
+
 }
 
-// to pass the password
-options := map[string]interface{}{
-   "password": password,        
+func main() {
+
+	for {
+		callHello()
+		time.Sleep(1 * time.Second)
+	}
+
 }
-
-// the login path
-// this is configurable, change userpass to ldap etc
-path := fmt.Sprintf("auth/userpass/login/%s", username)
-
-// PUT call to get a token
-secret, err := client.Logical().Write(path, options)
